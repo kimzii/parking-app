ff8cd242840514ffd3cbf03c669076180b6e9004
kimzii <kimzietorres@gmail.com>
2026-02-18 18:20:12 +0800

    feat(layout): wrap Stack component in SafeAreaView for improved layout handling

--- apps/mobile/app/(modals)/_layout.tsx
+++ apps/mobile/app/(modals)/_layout.tsx
@@ -3,6 +3,7 @@ import { MaterialIcons } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
 import { useCallback } from "react";
 import * as SecureStore from "expo-secure-store";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function StackLayout() {
   const router = useRouter();
@@ -20,24 +21,26 @@ export default function StackLayout() {
   );
 
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: { backgroundColor: "#fff" },
-        headerTintColor: "#11796F",
-        headerTitle: "", // Hide the title
-        headerLeft: () => (
-          <TouchableOpacity
-            onPress={() => router.back()}
-            style={{ marginLeft: 4 }}
-          >
-            <MaterialIcons
-              name="arrow-back-ios-new"
-              size={24}
-              color="#11796F"
-            />
-          </TouchableOpacity>
-        ),
-      }}
-    />
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: "#fff" },
+          headerTintColor: "#11796F",
+          headerTitle: "", // Hide the title
+          headerLeft: () => (
+            <TouchableOpacity
+              onPress={() => router.back()}
+              style={{ marginLeft: 4 }}
+            >
+              <MaterialIcons
+                name="arrow-back-ios-new"
+                size={24}
+                color="#11796F"
+              />
+            </TouchableOpacity>
+          ),
+        }}
+      />
+    </SafeAreaView>
   );
 }
